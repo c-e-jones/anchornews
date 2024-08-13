7caa1e99af2ae962a824c7ee4e51284077231653
Carl Jones <saanotherperson@gmail.com>
2024-08-13 20:56:23 +0000

    changed model to include helptext and make datetimefield function

--- news/models.py
+++ news/models.py
@@ -8,13 +8,16 @@ PUBLISH_CONTROL = ((0, "Unpublished"), (1, "Published"))
 class Post(models.Model):
         # This is the data model for posts
     title = models.CharField(
-        max_length=150, unique_for_date='created-on'
+        max_length=150, unique=True,
+        help_text='What is the title of this article?'
     )
     slug = models.SlugField(
-        max_length=200, unique_for_date='created-on'
+        max_length=200, unique=True,
+        help_text="What is the slug for this article?"
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=True
+        User, on_delete=models.CASCADE, null=True,
+        help_text="Which user created this article?"
     )
     genre = models.CharField(
         max_length=50, choices=[
@@ -30,10 +33,10 @@ class Post(models.Model):
             ("Arts and Culture", "Arts and Culture"),
             ("Finance", "Finance"),
             ("Crime", "Crime")
-        ])
-    byline = models.CharField(max_length=150)
+        ], help_text="What genre of news is this article?", default="0")
+    byline = models.CharField(max_length=150, help_text="What is your byline?")
     body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(help_text="When will this be posted?")
+    updated_on = models.DateTimeField(auto_now=True, help_text="When was this updated?")
     objects = models.Manager()
     status = models.IntegerField(choices=PUBLISH_CONTROL, default=0)
