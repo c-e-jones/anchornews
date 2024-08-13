465eb33e35488ac03be7d5c95fe1da8ec49fe57c
Carl Jones <saanotherperson@gmail.com>
2024-08-13 21:22:51 +0000

    designed a comment and review model

--- news/models.py
+++ news/models.py
@@ -5,6 +5,19 @@ from django.contrib.auth.models import User
 
 PUBLISH_CONTROL = ((0, "Unpublished"), (1, "Published"))
 
+REVIEW_CHOICES = [
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+    ('7', '7'),
+    ('8', '8'),
+    ('9', '9'),
+    ('10', '10'), 
+]
+
 class Post(models.Model):
         # This is the data model for posts
     title = models.CharField(
@@ -40,3 +53,13 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True, help_text="When was this updated?")
     objects = models.Manager()
     status = models.IntegerField(choices=PUBLISH_CONTROL, default=0)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='commenter')
+    comment_title = models.CharField(max_length=150, help_text="What is the main theme of your comment?")
+    body = models.TextField(max_length=600, help_text="What do you want to say in your comment?")
+    review = models.CharField(max_length=2, choices=REVIEW_CHOICES, help_text="What do you rate this article out of 10, where 10 is the best?")
+    approved = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
