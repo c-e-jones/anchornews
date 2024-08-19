eb302e96a0ebace68e1dede7bdf7f3c94eab7b06
Carl Jones <saanotherperson@gmail.com>
2024-08-19 07:58:14 +0000

    missed the view from the url path

--- news/urls.py
+++ news/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('',
     views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.article, name='article'),
-    path('<slug:slug>/edit_comment/<int:comment:id>'),
+    path('<slug:slug>/edit_comment/<int:comment:id>',
+        views.comment_edit, name='comment_editor'),
     path()
 ]
