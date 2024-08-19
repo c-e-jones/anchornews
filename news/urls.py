6e008db52a5c29c535d34837d80e0ec0f1f49196
Carl Jones <saanotherperson@gmail.com>
2024-08-19 07:53:57 +0000

    added url for comment editor

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('',
     views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.article, name='article')
+    path('<slug:slug>/', views.article, name='article'),
+    path('<slug:slug>/edit_comment/<int:comment:id>'),
+    path()
 ]
