ccad0a62261321ac921e5c27392c628290d706a6
Carl Jones <saanotherperson@gmail.com>
2024-10-23 12:40:11 +0000

    basic test for login function and comment

--- news/tests.py
+++ news/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.messages import get_messages
@@ -9,12 +9,16 @@ from .forms import CommentForm
 
 class TestPostViews(TestCase):
     
+    # This will set up 2 accounts, and build a test post, allowing checks
+    # to be carried out.
+
     def setUp(self):
         self.user = User.objects.create_superuser(
             username="testusername",
             password="testpassword",
             email="testemail@email.com"
         )
+
         self.post = Post(
             title="test title", slug='test-title',
             author=self.user, genre="Local Government, Local Government",
@@ -23,9 +27,39 @@ class TestPostViews(TestCase):
         )
         self.post.save()
 
+        self.other_user=User.objects.create_user(
+            username="testusername2",
+            password="testpassword2",
+            email="testemail2@email.com"
+        )
+
     def test_article_page_with_comment_form(self):
         response = self.client.get(reverse('article', args=['test-title']))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"test title", response.content)
         self.assertIn(b"Test body", response.content)
         self.assertIsInstance(response.context['comment_form'], CommentForm)
+
+
+    # Tests for basic login functions
+
+    def test_post_comment(self):
+        self.client.login(
+            username="testusername", password="testpassword")
+        post_data = {
+            'body': 'wow'
+            }
+        response = self.client.post(reverse(
+            'article', args=['test-title']), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(
+            b'Comment submitted.',
+            response.content
+        )
+
+
+
+    # Test for whether a comment is posted appropriately
+
+
+    
