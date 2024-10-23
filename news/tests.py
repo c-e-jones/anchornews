4b5729b7ab2cbd4d0f43f4ff430fecffef7b5927
Carl Jones <saanotherperson@gmail.com>
2024-10-23 06:59:16 +0000

    needs created on field

--- news/tests.py
+++ news/tests.py
@@ -17,6 +17,16 @@ class TestPostViews(TestCase):
         self.post = Post(
             title="Test title", slug="test-title",
             author=self.user, genre="Local Government, Local Government",
-            byline="Test byline", body="Test body")
+            byline="Test byline", body="Test body",
+            created_on="2024-08-13")
         self.post.save()
 
+    def test_article_page_with_comment_form(self):
+        response = self.client.get(reverse(
+            'article', args=['test-title']))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Test title", response.content)
+        self.assertIn(b"Test body", response.content)
+        self.assertIsInstance(
+            response.context['comment_form', CommentForm]
+        )
