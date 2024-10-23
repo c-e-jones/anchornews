59375cf01ad6504094d83e68455063f1e97564d4
Carl Jones <saanotherperson@gmail.com>
2024-10-23 07:22:03 +0000

    still cannot figure out the reverse args

--- news/tests.py
+++ news/tests.py
@@ -15,7 +15,7 @@ class TestPostViews(TestCase):
             email="testemail@email.com"
         )
         self.post = Post(
-            title="Test title", slug='test',
+            title="test title", slug='test-title',
             author=self.user, genre="Local Government, Local Government",
             byline="Test byline", body="Test body",
             created_on="2024-08-13"
@@ -23,8 +23,8 @@ class TestPostViews(TestCase):
         self.post.save()
 
     def test_article_page_with_comment_form(self):
-        response = self.client.get(reverse('article', args=['test']))
+        response = self.client.get(reverse('article', args=['test-title']))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Test title", response.content)
+        self.assertIn(b"test title", response.content)
         self.assertIn(b"Test body", response.content)
         self.assertIsInstance(response.context['comment_form'], CommentForm)
