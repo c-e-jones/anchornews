f7c9e17420e0bef0f1659977512ba690de535168
Carl Jones <saanotherperson@gmail.com>
2024-10-23 07:06:38 +0000

    testing if article is needed for url format

--- news/tests.py
+++ news/tests.py
@@ -23,7 +23,7 @@ class TestPostViews(TestCase):
 
     def test_article_page_with_comment_form(self):
         response = self.client.get(reverse(
-            'article', args=['test-title']))
+            args=['test-title']))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Test title", response.content)
         self.assertIn(b"Test body", response.content)
